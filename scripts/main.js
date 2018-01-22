b95db9833e7aab4a18891c087f8528ce3bfdbb9f
Sameera Shaakunthala <sameera.shaakunthala@pearson.com>
2018-01-23 00:09:26 +0530

    Just playin' around...

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,28 @@
-$(".menuitem").click(function () {
-   alert("Clicked"); 
+function showMenu (show) {
+    if (show == true) {
+        $('#expandablemenu').css ('visibility', 'visible');
+        $('#menuicon').css ('border-bottom', '2px solid #555');
+    } else {
+        $('#expandablemenu').css ('visibility', 'hidden');
+        $('#menuicon').css ('border-bottom', '2px solid #fff');
+    }
+}
+
+function initializeViewport () {
+    // Viewport parameters
+}
+
+$('#menuicon').click (function () {
+    var v = $('#expandablemenu').css('visibility');
+    if (v == 'hidden') {
+        showMenu(true);
+    } else {
+        showMenu(false);
+    }
 });
+
+$('.textlink').click (function () {
+    // Do it's job
+    showMenu(false);
+});
+
